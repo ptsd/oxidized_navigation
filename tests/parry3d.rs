23fed3d6444e8abcdcf5399a96a40a7c8a3fda0c
Patrick Dobbs <ptsd@patrickdobbs.co.uk>
2024-12-02 16:59:17 +0000

    CI: WIP

--- tests/parry3d.rs
+++ tests/parry3d.rs
@@ -127,3 +127,29 @@ fn test_simple_navigation() {
         panic!("Pathfinding failed: {error:?}");
     }
 }
+
+#[test]
+fn test_annotations() {
+    let mut app = App::new();
+
+    setup_app(&mut app);
+
+    app.add_systems(Startup, setup_world_system);
+
+    wait_for_generation_to_finish(&mut app);
+
+    assert_eq!(0, 0);
+}
+
+#[test]
+fn test_failure() {
+    let mut app = App::new();
+
+    setup_app(&mut app);
+
+    app.add_systems(Startup, setup_world_system);
+
+    wait_for_generation_to_finish(&mut app);
+
+    assert_eq!(0, 1, "{} must equal {}", 0, 1);
+}
